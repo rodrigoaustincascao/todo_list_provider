c8b3a8b43e5da7cddd650cbe4807fdda19421985
rodrigoaustincascao <rodrigoaustincascao@gmail.com>
2025-05-24 12:35:06 -0300

    Fix googleSignIn

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -14,12 +14,12 @@ android {
     ndkVersion = "28.1.13356709"
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_11.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
